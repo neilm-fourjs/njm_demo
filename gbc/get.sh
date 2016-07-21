a27b6f9c8c4906ceb65bd0789cae4fb5611cf7f8
Neil J Martin <neilm@4js.com>
2016-07-21 12:00:16 +0100

    tidy colours

---
+++ gbc/get.sh
@@ -0,0 +1,7 @@
+
+VER=20
+
+cp /opt/fourjs/gas300/njm-js/gwc-js-1.00.$VER/njm_cust/resources/img/* njm_cust/resources/img/
+cp /opt/fourjs/gas300/njm-js/gwc-js-1.00.$VER/njm_cust/js/* njm_cust/js/
+cp /opt/fourjs/gas300/njm-js/gwc-js-1.00.$VER/njm_cust/sass/* njm_cust/sass/
+cp /opt/fourjs/gas300/njm-js/gwc-js-1.00.$VER/njm_cust/theme.scss.json njm_cust/.
--- gbc/njm_cust/get.sh
+++
@@ -1,7 +0,0 @@
-
-VER=20
-
-cp /opt/fourjs/gas300/njm-js/gwc-js-1.00.$VER/njm_cust/resources/img/* resources/img/
-cp /opt/fourjs/gas300/njm-js/gwc-js-1.00.$VER/njm_cust/js/* js/
-cp /opt/fourjs/gas300/njm-js/gwc-js-1.00.$VER/njm_cust/sass/* sass/
-cp /opt/fourjs/gas300/njm-js/gwc-js-1.00.$VER/njm_cust/theme.scss.json .
